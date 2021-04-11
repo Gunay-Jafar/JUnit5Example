d8230d8c6f8006f412f02e88c04459cf9755e4e6
Gunay-Jafar <cafarligunay06@gmail.com>
2021-04-12 03:44:27 +0400

    Test performance

--- src/test/java/com/example/healthycoderapp/BMICalculatorTest.java
+++ src/test/java/com/example/healthycoderapp/BMICalculatorTest.java
@@ -1,7 +1,9 @@
 package com.example.healthycoderapp;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
 
+import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -27,16 +29,29 @@ public class BMICalculatorTest {
     }
 
     @Test
+    void returnCoderWorst() {
+        List<Coder> coders = new ArrayList<>();
+        for (int i = 0; i < 10000; i++) {
+            coders.add(new Coder(1.0 + i, 10.0 + i));
+        }
+
+        Executable executable = () -> BMICalculator.findCoderWithWorstBMI(coders);
+
+        assertTimeout(Duration.ofMillis(500),executable);
+    }
+
+
+    @Test
     void BMIScoreArray_coderListNotEmpty() {
 
         List<Coder> coders = new ArrayList<>();
         coders.add(new Coder(1.80, 60.0));
         coders.add(new Coder(1.82, 98.0));
         coders.add(new Coder(1.82, 64.7));
-        double[] expected = {18.52,29.59,19.53};
+        double[] expected = {18.52, 29.59, 19.53};
 
         double[] bmiScores = BMICalculator.getBMIScores(coders);
 
-        assertArrayEquals(expected,bmiScores);
+        assertArrayEquals(expected, bmiScores);
     }
 }
